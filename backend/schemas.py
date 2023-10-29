bef3fbe43f2608a5d51fe2f182cd76e891a5559d
thjungers <thomasjungers@gmail.com>
2023-10-29 18:13:27 +0100

    Backend: create schemas for orders

--- backend/schemas.py
+++ backend/schemas.py
@@ -44,3 +44,51 @@ class MenuItem(BaseModel):
 
     class Config:
         from_attributes = True
+
+
+class OrderOptionCreate(BaseModel):
+    """An option for an item in an order to create."""
+    option_slug: str
+    value: str
+
+class OrderOption(BaseModel):
+    """An option for an item in an order in the database."""
+    id: int
+    value: str
+    option: Option
+
+class OrderOptionDeep(OrderOption):
+    """An option for an item in an order in the database, with a reference to that item."""
+    order_item: "OrderItem"
+
+
+class OrderItemCreate(BaseModel):
+    """An item in an order to create."""
+    menu_item_id: int
+    options: list[OrderOptionCreate]
+
+class OrderItem(BaseModel):
+    """An item in an order in the database."""
+    id: int
+    menu_item: MenuItem
+    options: list[OrderOption]
+
+class OrderItemDeep(OrderItem):
+    """An item in an order in the database, with a reference to that order."""
+    order: "Order"
+
+
+class OrderCreate(BaseModel):
+    """An order to create."""
+    client_name: str
+    table_slug: str
+    items: list[OrderItemCreate]
+
+class Order(BaseModel):
+    """An order in the database."""
+    id: int
+    client_name: str
+    started: bool
+    served: bool
+    table: Table
+    items: list[OrderItem]
